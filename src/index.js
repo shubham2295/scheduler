b48d345ec3ec6d2ad71da7bf1df82628dd1dee93
Shubham <shubhampatel2295@gmail.com>
2022-04-27 15:48:05 -0600

    Added netlify configs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import "index.scss";
-
+import axios from "axios";
 import Application from "components/Application";
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
+
 
 ReactDOM.render(<Application />, document.getElementById("root"));
